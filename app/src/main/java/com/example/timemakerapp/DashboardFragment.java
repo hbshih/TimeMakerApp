0bf56032c16eb01a0e5e29596b5fca612f0a3ed5
Annkatrin <annkatrin@Annkatrins-MacBook-Pro.local>
2019-11-17 10:13:34 +0200

    dashboard layout

--- app/src/main/java/com/example/timemakerapp/DashboardFragment.java
+++ app/src/main/java/com/example/timemakerapp/DashboardFragment.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -13,14 +15,20 @@ import android.view.ViewGroup;
 
 public class DashboardFragment extends Fragment {
 
+    private CardView dailyFocusCard;
+
     public DashboardFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
     }
 
     @Override
