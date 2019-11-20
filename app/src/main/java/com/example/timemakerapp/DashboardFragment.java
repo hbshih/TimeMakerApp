bf2b1e6a9b88e21bfd2b8223cf0247afed98b795
Annkatrin <annkatrin@Annkatrins-MacBook-Pro.local>
2019-11-20 10:42:33 +0100

    dashboard layout and functionality

--- app/src/main/java/com/example/timemakerapp/DashboardFragment.java
+++ app/src/main/java/com/example/timemakerapp/DashboardFragment.java
@@ -1,21 +1,36 @@
 package com.example.timemakerapp;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.cardview.widget.CardView;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.LinkedList;
 
 
 public class DashboardFragment extends Fragment {
 
-    private CardView dailyFocusCard;
+    private CardView mDailyFocusCard;
+    private RecyclerView mRecyclerView;
+    private TextView mDailyFocus;
+    private CheckBox mDailyTaskCheckbox;
+    private PrevTaskListAdapter mAdapter;
+    private final LinkedList<String> mPreviousTasks = new LinkedList<String>();
 
     public DashboardFragment() {
         // Required empty public constructor
@@ -24,6 +39,11 @@ public class DashboardFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //TODO: Get list of previous tasks from database
+        mPreviousTasks.add("Make storyboard");
+        mPreviousTasks.add("Write report");
+
     }
 
     @Override
@@ -35,9 +55,43 @@ public class DashboardFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        final Context context = this.getContext();
 
+        mRecyclerView = view.findViewById(R.id.recyclerview);
+        mAdapter = new PrevTaskListAdapter(context, mPreviousTasks);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(context));
+
+        mDailyFocus = view.findViewById(R.id.daily_focus_text);
+        mDailyTaskCheckbox = view.findViewById(R.id.checkBox);
+        mDailyTaskCheckbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,
+                                         boolean isChecked) {
+                if (isChecked) {
+                    new AlertDialog.Builder(context)
+                            .setTitle("So close!")
+                            .setMessage("Have you completed today's focus?")
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int whichButton) {
+                                    mDailyTaskCheckbox.setVisibility(View.INVISIBLE);
+                                    mDailyFocus.setText(R.string.daily_task_achieved);
+                                    Toast.makeText(context, "Nice job!", Toast.LENGTH_SHORT).show();
+                                }})
+                            .setNegativeButton(android.R.string.no, null).show();
+
+                    mDailyTaskCheckbox.setChecked(false);
+                }
+            }
+        });
+
+
+        //TODO: Get current task from database
+
+        return view;
+    }
 
 
 }
