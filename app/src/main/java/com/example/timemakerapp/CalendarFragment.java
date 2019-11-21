3f029bac76039e6e7374e4a8f591cc72fe9dffb6
NaldiPramudita <naldipramudita@gmail.com>
2019-11-21 20:24:13 +0200

    Adding Fragment Calendar Design

--- app/src/main/java/com/example/timemakerapp/CalendarFragment.java
+++ app/src/main/java/com/example/timemakerapp/CalendarFragment.java
@@ -1,6 +1,7 @@
 package com.example.timemakerapp;
 
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,6 +9,22 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.List;
+
+import javax.annotation.Nonnull;
 
 
 /**
@@ -15,10 +32,66 @@ import android.view.ViewGroup;
  */
 public class CalendarFragment extends Fragment {
 
+    private DatabaseReference mDatabase;
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    ListView listview;
+    Context mC;
+    private DatabaseReference myRef = database.getReference().child("achievements");
+
+    //Connection to Firebase Firestore to get achievements datas
+    private void getAchievementsItems() {
+
+        FirebaseFirestore.getInstance()
+                .collection(
+                        "users")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@Nonnull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            List<DocumentSnapshot> myListOfDocuments = task.getResult().getDocuments();
+                            System.out.println("OnSuccess : " + myListOfDocuments);
+                        }else
+                        {
+                            System.out.println("Query Failed");
+                        }
+                    }
+                });
+
+    }
+
+    //Connectionn to Firebase Database
+    private void readRealtimeDatabaseValue(){
+
+        System.out.println("Check Database Value");
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                System.out.println("Realtime Database Value is " + value);
+                // Log.d(, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                System.out.println("Database Error " + error);
+                // Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+    }
+
+
+
 
     public CalendarFragment() {
         // Required empty public constructor
     }
+
 
 
     @Override
