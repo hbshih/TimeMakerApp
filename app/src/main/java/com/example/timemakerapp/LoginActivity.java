f9af37d672f18c6e9f39f46cc93095b7a60e2047
Mario <mario.lopez.batres@gmail.com>
2019-11-28 13:09:51 +0100

    Firebase Login by email working

--- app/src/main/java/com/example/timemakerapp/LoginActivity.java
+++ app/src/main/java/com/example/timemakerapp/LoginActivity.java
@@ -1,17 +1,30 @@
 package com.example.timemakerapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+    // Firebase
+    private FirebaseAuth mAuth;
+
+    // UI Elements
     EditText email;
     EditText password;
     Button login;
@@ -39,8 +52,7 @@ public class LoginActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(loginIntent);
+                onSingIn(email.getText().toString(), password.getText().toString());
             }
         });
         google.setOnClickListener(new View.OnClickListener() {
@@ -65,4 +77,29 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void onSingIn(String email, String password) {
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        // Sing in existing users
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(loginIntent);
+                            // FirebaseUser user = mAuth.getCurrentUser();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
 }
