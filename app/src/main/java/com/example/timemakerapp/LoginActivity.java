c4daa9d0a363a34b9b8d48bb927c65ff5048f500
Mario <mario.lopez.batres@gmail.com>
2019-11-28 17:41:56 +0100

    No action bar in LoginActivity and RegisterActivity

--- app/src/main/java/com/example/timemakerapp/LoginActivity.java
+++ app/src/main/java/com/example/timemakerapp/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.timemakerapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
@@ -47,6 +48,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        // Hide Keyboad
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
 
         // Init
