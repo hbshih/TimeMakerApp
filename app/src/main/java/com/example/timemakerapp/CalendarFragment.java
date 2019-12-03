ab80ea7628c6cdd00e8245d630cc09cbe228da07
NaldiPramudita <naldipramudita@gmail.com>
2019-12-03 14:07:20 +0100

    calendar icon logic

--- app/src/main/java/com/example/timemakerapp/CalendarFragment.java
+++ app/src/main/java/com/example/timemakerapp/CalendarFragment.java
@@ -1,18 +1,26 @@
 package com.example.timemakerapp;
 
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.text.format.DateUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
 import android.widget.ListView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -20,8 +28,11 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
 
 import javax.annotation.Nonnull;
@@ -32,6 +43,8 @@ import android.graphics.Color;
 import android.graphics.Color;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -39,12 +52,12 @@ import com.example.timemakerapp.Utils.DrawableUtils;
 import com.applandeo.materialcalendarview.CalendarView;
 import com.applandeo.materialcalendarview.EventDay;
 import com.applandeo.materialcalendarview.exceptions.OutOfDateRangeException;
-import com.applandeo.materialcalendarview.utils.DateUtils;
 
 
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 
@@ -54,108 +67,109 @@ import java.util.Random;
  */
 public class CalendarFragment extends Fragment {
 
-    private DatabaseReference mDatabase;
-    private FirebaseDatabase database = FirebaseDatabase.getInstance();
-    ListView listview;
-    Context mC;
-    private DatabaseReference myRef = database.getReference().child("achievements");
-
-    //Connection to Firebase Firestore to get achievements datas
-    private void getAchievementsItems() {
-
-        FirebaseFirestore.getInstance()
-                .collection(
-                        "users")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@Nonnull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            List<DocumentSnapshot> myListOfDocuments = task.getResult().getDocuments();
-                            System.out.println("OnSuccess : " + myListOfDocuments);
-                        }else
-                        {
-                            System.out.println("Query Failed");
-                        }
-                    }
-                });
-
-    }
-
-    //Connectionn to Firebase Database
-    private void readRealtimeDatabaseValue(){
-
-        System.out.println("Check Database Value");
-
-        // Read from the database
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
-                System.out.println("Realtime Database Value is " + value);
-                // Log.d(, "Value is: " + value);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                System.out.println("Database Error " + error);
-                // Log.w(TAG, "Failed to read value.", error.toException());
-            }
-        });
-    }
-
-
-
+    private FirebaseFirestore db;
+    private String currentUser;
+    private DailyTask currentTask;
+    private final LinkedList<DailyTask> allTasks = new LinkedList<DailyTask>();
 
     public CalendarFragment() {
         // Required empty public constructor
     }
 
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_calendar,container,false);
+        //get db instance
+        db = FirebaseFirestore.getInstance();
+        //get current user
+        currentUser = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        Log.d("userid", currentUser);
 
+
+        View view = inflater.inflate(R.layout.fragment_calendar, container, false);
         List<EventDay> events = new ArrayList<>();
 
-        Calendar calendar = Calendar.getInstance();
-        events.add(new EventDay(calendar, R.drawable.sample_three_icons));
-//
- //       events.add(new EventDay(calendar, new Drawable()));
-//or if you want to specify event label color
-        events.add(new EventDay(calendar, R.drawable.sample_three_icons, Color.parseColor("#228B22")));
+        db.collection("tasks")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
 
-        Calendar calendar1 = Calendar.getInstance();
-        calendar1.add(Calendar.DAY_OF_MONTH, 10);
-        events.add(new EventDay(calendar1, R.drawable.sample_icon_2));
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()){
+                            for (QueryDocumentSnapshot doc : task.getResult()){
+                                Map<String,Object> taskMap = doc.getData();
+                                String taskUser = (String) taskMap.get("userId");
 
-        Calendar calendar2 = Calendar.getInstance();
-        calendar2.add(Calendar.DAY_OF_MONTH, 5);
-        events.add(new EventDay(calendar2, R.drawable.sample_icon_3));
+                                if (!currentUser.equals(taskUser)){
+                                    continue;
+                                }
+                                String taskName = (String) taskMap.get("name");
+                                Date taskTime = ((Timestamp) taskMap.get("time")).toDate();
+                                boolean isAchieved = (boolean) taskMap.get("achieved");
+                                DailyTask dailyTask = new DailyTask(taskName, taskTime, isAchieved, currentUser);
+                                dailyTask.setId(doc.getId());
 
-        Calendar calendar3 = Calendar.getInstance();
-        calendar3.add(Calendar.DAY_OF_MONTH, 7);
-        events.add(new EventDay(calendar3, R.drawable.sample_four_icons));
+                                allTasks.add(dailyTask);
 
-        CalendarView calendarView = (CalendarView) view.findViewById(R.id.calendarView);
+                                Log.d("TaskRead", "TASK READ SUCCESS");
+                                Log.d("TaskRead", taskName);
 
-        Calendar min = Calendar.getInstance();
-        min.add(Calendar.MONTH, -2);
 
-        Calendar max = Calendar.getInstance();
-        max.add(Calendar.MONTH, 2);
+                            }
+                            //putting in events
+                            for(int num=0; num<allTasks.size(); num++)
+                            {
+                                DailyTask taskEvent = getTasks(allTasks,num);
+                                if (taskEvent.isAchieved() == true){
+                                    addSuccessEvents(events,taskEvent.getTime());
+                                    System.out.println(taskEvent.getTime());
+                                    System.out.println("success added");
 
-        calendarView.setMinimumDate(min);
-        calendarView.setMaximumDate(max);
+                                }
+                                else
+                                {
+                                    addFailedEvents(events,taskEvent.getTime());
+                                    System.out.println("failed added");
+                                }
+                                System.out.println(allTasks.get(num));
+                            }
 
-        calendarView.setEvents(events);
+
+                        }
+
+                        else {
+                            Log.d("TaskRead", "DailyTask failed reading");
+                        }
+                    }
+                });
+
 
         return view;
+    }
+
+    private void addSuccessEvents(List<EventDay> events, Date date) {
+
+        Calendar calendar1 = Calendar.getInstance();
+        calendar1.set(date.getYear()+1900,date.getMonth(),date.getDate());
+        events.add(new EventDay(calendar1, R.drawable.sample_icon_1));
+        System.out.println("success added, masuk ke voidnya");
+
+        System.out.println("dalem void"+ date.getYear()+ "YY" + date.getMonth() + "MM" + date.getDate());
+
+
+
+    }
+
+    private void addFailedEvents(List<EventDay> events,Date date) {
+        Calendar calendar2 = Calendar.getInstance();
+        calendar2.set(date.getYear()+1900,date.getMonth(),date.getDate());
+        events.add(new EventDay(calendar2, R.drawable.sample_icon_2));
+    }
+
+    private DailyTask getTasks(LinkedList<DailyTask> TaskList, int num){
+        DailyTask task = TaskList.get(num);
+        return task;
     }
 
 }
